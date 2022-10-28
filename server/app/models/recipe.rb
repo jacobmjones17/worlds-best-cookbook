b8bbcac9201844472e7845c571da16be87b15569
jacob <jacobmjones17@gmail.com>
2022-10-28 01:19:16 -0400

    I added a new table of reviews and I created all of the models and created their associations

--- server/app/models/recipe.rb
+++ server/app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+    has_many :ingredients
+    has_many :reviews
+    has_many :users, through: :reviews
 end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :reviews
+    has_many :recipes, through: :reviews
+    
+    validates :username, uniqueness: true
+    validates :username, presence: true
 end
