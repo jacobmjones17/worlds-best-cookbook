bf32dcddf1b5d2812c7acb871bc5088b9b4b6ca0
jacob <jacobmjones17@gmail.com>
2022-11-18 22:55:26 -0500

    Done

--- server/app/models/recipe.rb
+++ server/app/models/recipe.rb
@@ -6,23 +6,21 @@ class Recipe < ApplicationRecord
     validates :name, uniqueness: true
 
     def recipe_ingredients_attributes=(array)
-        array.each do |hash|
-          
-          if hash.has_key? (:id)
-            ingredient = Ingredient.find(hash["ingredient_id"])
-            ingredient.update(name: hash["ingredient"])
-            recipe_ingredient = RecipeIngredient.find(hash["id"])
-            recipe_ingredient.update(measurement: hash["measurement"], ingredient_id: ingredient.id)
-            
-            self.recipe_ingredients_ids.for_each do |id|
-              byebug
-              updated_ids = array.map { |object| object.id}
-              RecipeIngredient.find(id).delete if !updated_ids.includes(id)
-            end
-          else
-            new_ingredient = Ingredient.find_or_create_by(name: hash["ingredient"])
-            self.recipe_ingredients.build(ingredient_id: new_ingredient.id, measurement: hash["measurement"])
-          end
+      array.each do |hash|
+
+        if hash.has_key? (:id)
+          updated_ids = array.map { |object| object[:id]}.delete_if(&:nil?)
+          self.recipe_ingredients.each {|obj| obj.delete if !updated_ids.include?(obj.id)}
+
+          ingredient = Ingredient.find(hash["ingredient_id"])
+          ingredient.update(name: hash["ingredient"])
+          recipe_ingredient = RecipeIngredient.find(hash["id"])
+          recipe_ingredient.update(measurement: hash["measurement"], ingredient_id: ingredient.id)
+
+        else
+          new_ingredient = Ingredient.find_or_create_by(name: hash["ingredient"])
+          self.recipe_ingredients.build(ingredient_id: new_ingredient.id, measurement: hash["measurement"])
         end
-    end
+      end
+  end
 end
