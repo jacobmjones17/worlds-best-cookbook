371a41aeb2a3d756a462b2a269f4beb9b9446061
Jacob <jacobmjones17@gmail.com>
2022-11-20 18:26:21 -0500

    fixed more CSS styling

--- client/src/components/ingredients/AddIngredient.js
+++ client/src/components/ingredients/AddIngredient.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Ingredient.css"
 
 function AddIngredient ({ handleIngredientAdd, handleIngredientChange, handleIngredientRemove, ingredientList }) {
 
--- client/src/components/ingredients/EditIngredient.js
+++ client/src/components/ingredients/EditIngredient.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Ingredient.css"
 
 function EditIngredient ({ handleIngredientAdd, recipeIngredientList, handleIngredientChange, handleIngredientRemove }) {
     
